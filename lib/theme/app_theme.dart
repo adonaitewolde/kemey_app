2e3d27c703eb113a667f0661c8d0816732920c3d
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2026-01-18 18:40:05 +0100

    feat: add primaryBrown color to AppColors for enhanced theming options

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,4 +5,5 @@ class AppColors {
 
   static const Color primaryOrange = Color(0xFFFF8000);
   static const Color primaryOrangeDark = Color(0xFFFF6400);
+  static const Color primaryBrown = Color(0xFF442510); // Color.fromARGB(255, 68, 37, 16)
 }
